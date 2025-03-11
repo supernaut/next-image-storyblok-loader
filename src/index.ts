b66e758fb917e8d73f5c619d9c028aae826c4e0b
Johannes <johannes@axner.io>
2025-03-11 22:58:35 +0100

    refactor: split entry and use more modern config

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { storyblokImageLoader } from "./lib/storyblok-image-loader";
-export { getStoryblokImageLoader } from "./lib/get-storyblok-image-loader";
-export { imgSrcIsStoryblok } from "./lib/img-src-is-storyblok";
-export type { StoryblokImageLoaderOptions } from "./types/storyblok-image-loader-options";
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: [
+    "./src/lib/storyblok-image-loader.ts",
+    "./src/lib/get-storyblok-image-loader.ts",
+    "./src/lib/img-src-is-storyblok.ts",
+    "./src/types/storyblok-image-loader-options.ts",
+  ],
   splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
-  format: ['esm'],
+  platform: "neutral",
+  format: ["esm"],
 });
